8a5d9b29e8725a28c82b58fd182bc823980cab98
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-13 21:56:41 +0600

    layout

--- src/Cart.js
+++ src/Cart.js
@@ -5,7 +5,7 @@ import ProductCard from './ProductCard';
 const Cart = () => {
     const cart = useSelector((state) => state.cart)
     return (
-        <div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
             {
                 cart.map(product => <ProductCard product={product} ></ProductCard>)
             }

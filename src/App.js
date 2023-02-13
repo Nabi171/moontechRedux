86bf76aaa2f7797a7081f69de68fce6a46b25487
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-13 15:43:22 +0600

    store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
-
+import { Provider } from "react-redux"
+import store from './redux/store';
 function App() {
   return (
     <div className="container mx-auto ">
-      <Navbar></Navbar>
-      <Home></Home>
+      <Provider store={store} >
+        <Navbar></Navbar>
+        <Home></Home>
+      </Provider>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import ProductCard from './ProductCard';
-
+import { useSelector } from "react-redux";
 const Home = () => {
     const [products, setProducts] = useState([]);
-    fetch("products.json").then((res) => res.json()).then((data) => setProducts(data))
+    fetch("products.json").then((res) => res.json()).then((data) => setProducts(data));
+
+    const state = useSelector((state) => state);
+
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
             {

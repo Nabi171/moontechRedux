587b648ed863f990981ca7993f3472ac428829b0
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-13 16:07:52 +0600

    added sor

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     fetch("products.json").then((res) => res.json()).then((data) => setProducts(data));
 
     const state = useSelector((state) => state);
-
+    console.log(state);
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
             {
--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BiListPlus } from "react-icons/bi";
 import { MdDeleteForever } from "react-icons/md";
+import { useDispatch } from 'react-redux';
+import { ADD_TO_CART } from './redux/actionTypes/actionTypes';
+import { addToCart } from './redux/actionCreators/productAction';
 const ProductCard = ({ product }) => {
+
+    const dispatch = useDispatch();
     return (
         <div className='shadow-lg bg-white relative rounded-3xl border p-3 flex flex-col text-indigo-900'>
 
@@ -28,7 +33,7 @@ const ProductCard = ({ product }) => {
             <div className='flex gap-2 mt-5'>
 
                 <button
-
+                    onClick={() => dispatch(addToCart(product))}
                     className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
                 >
                     Add to cart
